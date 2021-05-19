63e4c6015531abae682617dd711d9ad0457e671d
Linchen <ling199104@gmail.com>
2021-05-19 23:29:59 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ from discord import Member, Embed, FFmpegPCMAudio
 import os
 import traceback
 from gtts import gTTS
-from io import BytesIO
+from io import BytesIO, BufferedIOBase
 
 
 class Greetings(commands.Cog):
@@ -52,6 +52,7 @@ class Greetings(commands.Cog):
             fp = BytesIO()
             tts.write_to_fp(fp)
             fp.seek(0)
+            buffered_io_base = io.BufferedIOBase(fp)
             
             FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
             guild = ctx.guild
@@ -70,7 +71,7 @@ class Greetings(commands.Cog):
 #             await self.join(ctx, voice)
 #             voice_client: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=guild)
 #             voice_client.play(FFmpegPCMAudio(f, **FFMPEG_OPTS), after=lambda e: print('done', e))
-            voice.play(FFmpegPCMAudio(fp, **FFMPEG_OPTS), after=lambda e: print('done', e))
+            voice.play(FFmpegPCMAudio(buffered_io_base, **FFMPEG_OPTS), after=lambda e: print('done', e))
             voice.is_playing()
             f.close()
             
